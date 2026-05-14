46b6f37a0fe393d5f34c416d1d8b64bdb3746e18
Callum Jones <call_jones2@hotmail.com>
2026-05-14 13:10:51 +0200

    added theme colors, theme fonts as a variable in tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope, Yeseva_One } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const manrope = Manrope({
+  variable: "--font-manrope",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const yesevaOne = Yeseva_One({
+  weight: "400",
+  variable: "--font-yeseva",
   subsets: ["latin"],
-});
+}); 
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${manrope.variable} ${yesevaOne.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         {children}
