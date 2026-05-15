417b3cfaf55c9749d7ff7a48700160ec02530519
Oscar Öjling <oscarojling@gmail.com>
2026-05-15 13:46:42 +0200

    Changed sizing on logo for desktop and mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,10 +6,11 @@ const Header = () => {
       <Image
         src="/images/hss_logo.png"
         alt="HSS Logo"
-        width={80}
-        height={80}
+        width={68}
+        height={68}
         loading="eager"
-      ></Image>
+        className="w-13 h-13 md:w-17 md:h-17"
+      />
     </header>
   );
 };
