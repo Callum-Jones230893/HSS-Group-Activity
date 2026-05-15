36a707d3ace48ac679bf20b61636cad5da628a85
Callum Jones <call_jones2@hotmail.com>
2026-05-15 14:13:21 +0200

    added buttons, multiple sizes as props as well as color themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Button from "@/components/Button";
+
 export default function Home() {
   return (
     <>
       <h1 className="text-yellow font-yeseva">Hello World except Claudia</h1>
+      <Button theme="transparent" size="large" />
     </>
   );
 }
