13a4e42fc70e7fcc99741c617af23bd373599f90
Callum Jones <call_jones2@hotmail.com>
2026-05-15 11:30:00 +0200

    updated for production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import ColorTest from "@/components/ColorTest";
-
 export default function Home() {
   return (
     <>
       <h1 className="text-yellow font-yeseva">Hello World except Claudia</h1>
-      <ColorTest />
     </>
   );
 }
