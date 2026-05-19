b999ce7289ddc68b73bf539f684beb5eeae605f8
claudiacarion <claudia.carion@gmail.com>
2026-05-19 15:17:42 +0200

    add all styling for hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from '@/components/Hero';
 export default function Home() {
   return (
     <>
-      <Hero />
+      <Hero subtitle="Äventyr och kompisar!" title="Välkommen till Hässelby Strands Sjöscoutkår. Äventyr på riktigt!"/>
       <h1 className="text-yellow font-yeseva">Hello World except Claudia</h1>
     </>
   );
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -5,27 +5,30 @@ type HeroProps = {
   subtitle: string;
 };
 
-const Hero = () => {
+const Hero = ({ subtitle, title }: HeroProps) => {
   return (
     <div
-      style={{
-        backgroundImage: `url("/images/hero_background.png")`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-      }}
-      className="flex flex-col items-center">
-      <div className="flex flex-col items-center justify-center mt-48 w-188 mx-auto">
-        <h3 className="text-secondary font-primary text-[28px]">Äventyr och kompisar!</h3>
-        <h1 className="text-white font-secondary text-[40px] mt-8 mb-20.5 text-center">
-          Välkommen till Hässelby Strands Sjöscoutkår. Äventyr på riktigt!
-        </h1>
-        <div className="flex">
+      className="flex flex-col items-center h-screen justify-between
+      bg-[url('/images/hero_background.png')]
+      bg-no-repeat
+      bg-cover
+      bg-position-[-120px_center]
+      md:bg-center">
+      <div className="flex flex-col items-center justify-center mt-20.75 w-65.75 md:mt-36 md:w-188 mx-auto">
+        <h3 className="text-secondary font-primary md:text-[28px]">{subtitle}</h3>
+        <h1 className="text-white font-secondary text-[22px] md:text-[40px] mt-8 text-center">{title}</h1>
+        <div className="flex flex-col md:flex-row md:mt-20">
           <button className="bg-secondary p-2 m-4">BUTTON PLACEHOLDER</button>
           <button className="bg-secondary p-2 m-4">BUTTON PLACEHOLDER</button>
         </div>
       </div>
-      <Image src="/images/hss_brand.png" alt="HSS branding" width={457} height={109} style={{ opacity: 0.65 }} />
+      <Image
+        src="/images/hss_brand.png"
+        alt="HSS branding"
+        width={457}
+        height={109}
+        className="opacity-65 hidden md:block pb-4"
+      />
     </div>
   );
 };
