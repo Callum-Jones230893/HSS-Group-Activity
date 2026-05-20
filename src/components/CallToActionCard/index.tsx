953f91ca11001ba47eb1ac3c793cdaea4e99f2de
Callum Jones <call_jones2@hotmail.com>
2026-05-20 22:23:11 +0200

    updated font weight on title

--- src/components/CallToActionCard/index.tsx
+++ src/components/CallToActionCard/index.tsx
@@ -16,10 +16,10 @@ type CallToActionProps = {
 const CallToActionCard = ({item}: CallToActionProps) => {
   return (
     <div className="flex flex-col max-w-9/10 mx-auto min-h-223.25 text-primary font-primary p-4 md:flex-row 
-      md:max-w-387.25 md:h-186 md:mx-14.75 md:my-17 md:justify-center md:odd:flex-row-reverse"
+      md:max-w-387.25 md:h-186 md:my-17 md:justify-center md:odd:flex-row-reverse"
     >
       <div className="flex flex-col min-h-109.5 md:flex-col md:w-163.25 md:h-162.5 md:mx-22.5">
-        <h4 className="text-[24px] md:text-[36px]">{item.title}</h4>
+        <h4 className="text-[24px] font-semibold md:text-[36px]">{item.title}</h4>
         <h2 className="text-black text-[28px] md:text-[44px]">{item.subtitle}</h2>
         <p className="my-5 text-[18px] tracking-[1%] md:py-15.25 md:text-[24px]">{item.description}</p>
         <div className="flex justify-between items-center md:pb-17 text-[20px] md:text-[24px]">
