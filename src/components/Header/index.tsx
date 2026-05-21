0447d505ceeab2f50a4396fca011ab21e148a3f9
claudiacarion <claudia.carion@gmail.com>
2026-05-21 14:20:43 +0200

    add spaces to trigger merge conflict

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,15 +3,14 @@ import Navigation from '../Navigation';
 
 const Header = () => {
   return (
-    <header className= " bg-white flex items-center">
+    <header className= " bg-white flex items-center ">
       <Image
         src="/images/hss_logo.png"
         alt="HSS Logo"
         width={68}
         height={68}
         loading="eager"
-        className="w-13 h-13 m-4 md:w-17 md:h-17"
-      />
+        className="w-13 h-13 m-4 md:w-17 md:h-17"/>
       <Navigation />
       
     </header>
