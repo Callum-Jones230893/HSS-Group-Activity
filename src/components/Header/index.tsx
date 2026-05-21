cc19d0f80d23306434570def545cb92f3e5737ed
claudiacarion <claudia.carion@gmail.com>
2026-05-21 18:36:05 +0200

    fix spacing for tablet

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className=" bg-white flex items-center justify-center md:w-5/6 md:mx-auto">
+    <header className=" bg-white flex items-center justify-center lg:w-5/6 lg:mx-auto">
       <Link href="/" className="shrink-0 p-4">
         <Image
           src="/images/hss_logo.png"
--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -26,7 +26,7 @@ const Navigation = () => {
   }, [hamburger]);
 
   return (
-    <nav className="relative font-primary text-primary w-full md:max-w-4/5 md:mx-auto">
+    <nav className="relative font-primary text-primary w-full lg:max-w-4/5 lg:mx-auto">
       <div ref={menuRef}>
         <div className="flex justify-end m-4 lg:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none" onClick={() => setHamburger(prev => !prev)}>
@@ -42,7 +42,7 @@ const Navigation = () => {
         <div
           className={`${hamburger ? "flex" : "hidden"}
         bg-white py-4
-        absolute w-3/4 right-0 pr-4 lg:pr-0 h-screen flex-col items-end gap-8 overflow-hidden overscroll-none z-15
+        absolute w-2/3 md:w-1/2 right-0 pr-4 lg:pr-0 h-screen flex-col items-end gap-8 overflow-hidden overscroll-none z-15
         lg:h-auto lg:w-full lg:gap-2 lg:static lg:flex lg:flex-row lg:justify-between lg:items-center lg:overflow-visible`}>
           <NavMenu />
         </div>
