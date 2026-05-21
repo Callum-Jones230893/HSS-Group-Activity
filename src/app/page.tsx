58d0e9c921a5681b7e49409c69f5872f4f36f934
Callum Jones <call_jones2@hotmail.com>
2026-05-21 14:12:14 +0200

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <>
       <h1 className="text-yellow font-yeseva">Hello World except Claudia</h1>
-      <Button theme="transparent" size="large" />
     </>
   );
 }
