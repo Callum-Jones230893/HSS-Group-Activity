7c6e4d94415ba6fc250b867d91edfa3d7bf2450e
Callum Jones <call_jones2@hotmail.com>
2026-05-22 17:43:31 +0200

    updated with responsiveness

--- src/components/DepartmentCards/index.tsx
+++ src/components/DepartmentCards/index.tsx
@@ -7,10 +7,31 @@ type DepartmentItem = {
   image: string
 }
 
-const DepartmentCards = () => {
+type DepartmentItemProps = {
+  item: DepartmentItem
+}
+
+const DepartmentCards = ({item}: DepartmentItemProps) => {
   return (
-    <div>
-      <Image />
+    <div className="lg:flex flex-col lg:flex-row items-center rounded-lg lg:rounded-2xl lg:border-l-[3px] 
+      lg:border-r-[0.50px] lg:border-t-[0.50px] lg:border-b-[3px] lg:border-lime-600/50 mb-21.75 font-primary text-lg lg:text-2xl p-8
+      outline-[0.50px] outline-offset-[-0.50px] outline-lime-600/50 inline-flex"
+    >
+      <div className="flex w-7/10 lg:min-w-3/10">
+        <Image src={item.image} width="400" height="400" alt={item.title} className="size-28.5 lg:size-60 rounded-2xl"/>
+      </div>
+      <div className="flex flex-col my-3 lg:mx-11.5">
+        <div className="flex items-center gap-1.5">
+          <h3 className="font-extralight text-2xl lg:text-3xl text-sky-900">{item.title}</h3>
+          <p className="font-bold text-1xl">{item.subtitle}</p>
+        </div>
+        <div className="flex justify-start lg:pt-4.5 lg:pr-20 font-light text-slate-900 self-stretch leading-8 lg:leading-10 tracking-tight">
+          <p>{item.description}</p>
+        </div>
+      </div>
+      <div className="flex items-center justify-center min-h-1/10">
+        <p className="pt-4">hello</p>
+      </div>
     </div>
   )
 }
--- src/components/DepartmentSelection/index.tsx
+++ src/components/DepartmentSelection/index.tsx
@@ -1,4 +1,4 @@
-import DepartmentCards from "../DepartmentCards"
+import DepartmentCards from "@/components/DepartmentCards"
 
 type DepartmentItem = {
   title: string
@@ -9,25 +9,25 @@ type DepartmentItem = {
 
 const DepartmentData: DepartmentItem[] = [
   {
-    title: "Sjöhumlor -",
+    title: "Sjöhumlor - ",
     subtitle: "Spårare 8-9 år",
     description: "vid 8 års ålder kan man börja i HSS, dvs när man börjar i årskurs 2. Man blir då Spårarscout och medlem i avdelningen Sjöhumlorna. (Tidigare hette spårarscout ”miniorscout”) Vi har möten varje vecka samt övernattningar, både hajker och längre läger",
     image: "/images/department1.png"
   },
   {
-    title: "Kaparna -",
+    title: "Kaparna - ",
     subtitle: "Spårare 10-11 år",
     description: "På Kaparna börjar man vid 10 års ålder och stannar kvar i två år tills det är dags för nästa steg. (Tidigare hette upptäckarscout ”juniorscout”) På Kaparna stegras svårighetsgraden på uppgifterna.",
     image: "/images/department2.png"
   },
   {
-    title: "Konvojen -",
+    title: "Konvojen - ",
     subtitle: " Äventyrare 12-14 år",
     description: "Vid 12 års ålder öppnas dörrarna till Äventyrarscouterna. I tre spännande år får man lära sig nyttiga saker inom sjömansskap och natur. (Tidigare hette äventyrarscout ”patrullscout”)",
     image: "/images/department3.png"
   },
   {
-    title: "Utmanare -",
+    title: "Utmanare - ",
     subtitle: "15+ år",
     description: "HSS utmanarscoutlag upphör aldrig att existera! Efter tiden som utmanarscout är man ett tätt sammansvetsat gäng. Det är inte ovanligt att man håller kontakten livet ut. (Tidigare hette denna avdelning ”seniorscout”)",
     image: "/images/department4.png"
@@ -42,10 +42,12 @@ const DepartmentData: DepartmentItem[] = [
 
 const DepartmentSelection = () => {
   return (
-    <section className="w-8/10 h-3/10 bg-amber-50 mx-auto">
-      {DepartmentData.map((item, index) => {
-        <DepartmentCards key={index} item={item} />
-      })}
+    <section className="my-22">
+      <div className="flex flex-col w-9/10 lg:w-7/10 mx-auto">
+        {DepartmentData.map((item, index) => 
+          <DepartmentCards key={index} item={item} />
+        )}
+      </div>
     </section>
   )
 }
