45dc2775069e60b7eec5b479927d91331ca69f3f
Callum Jones <call_jones2@hotmail.com>
2026-05-22 18:49:06 +0200

    added content and sizing, needs dynamic coloring

--- src/components/DepartmentCards/index.tsx
+++ src/components/DepartmentCards/index.tsx
@@ -5,6 +5,7 @@ type DepartmentItem = {
   subtitle: string
   description: string
   image: string
+  color: string
 }
 
 type DepartmentItemProps = {
@@ -12,20 +13,21 @@ type DepartmentItemProps = {
 }
 
 const DepartmentCards = ({item}: DepartmentItemProps) => {
+
   return (
-    <div className="lg:flex flex-col lg:flex-row items-center rounded-lg lg:rounded-2xl lg:border-l-[3px] 
-      lg:border-r-[0.50px] lg:border-t-[0.50px] lg:border-b-[3px] lg:border-lime-600/50 mb-21.75 font-primary text-lg lg:text-2xl p-8
-      outline-[0.50px] outline-offset-[-0.50px] outline-lime-600/50 inline-flex"
+    <div className={`xl:flex flex-col xl:flex-row items-center rounded-lg xl:rounded-2xl lg:border-l-[3px] 
+      lg:border-r-[0.50px] lg:border-t-[0.50px] lg:border-b-[3px] lg:border-${} mb-21.75 font-primary text-lg xl:text-2xl p-8
+      outline-[0.50px] outline-offset-[-0.50px] outline-${} inline-flex`}
     >
-      <div className="flex w-7/10 lg:min-w-3/10">
-        <Image src={item.image} width="400" height="400" alt={item.title} className="size-28.5 lg:size-60 rounded-2xl"/>
+      <div className="flex min-w-7/10 xl:min-w-[20%]">
+        <Image src={item.image} width="400" height="400" alt={item.title} loading="eager" className="size-28.5 xl:size-60 rounded-2xl"/>
       </div>
-      <div className="flex flex-col my-3 lg:mx-11.5">
+      <div className="flex flex-col my-3 xl:mx-11.5 justify-evenly">
         <div className="flex items-center gap-1.5">
-          <h3 className="font-extralight text-2xl lg:text-3xl text-sky-900">{item.title}</h3>
-          <p className="font-bold text-1xl">{item.subtitle}</p>
+          <h3 className="font-extralight text-2xl xl:text-3xl text-sky-900">{item.title}</h3>
+          <p className={`font-bold text-1xl text-${}`}>{item.subtitle}</p>
         </div>
-        <div className="flex justify-start lg:pt-4.5 lg:pr-20 font-light text-slate-900 self-stretch leading-8 lg:leading-10 tracking-tight">
+        <div className="flex justify-start pt-4.5 xl:pr-20 font-light text-slate-900 self-stretch leading-8 xl:leading-10 tracking-tight">
           <p>{item.description}</p>
         </div>
       </div>
--- src/components/DepartmentSelection/index.tsx
+++ src/components/DepartmentSelection/index.tsx
@@ -5,6 +5,7 @@ type DepartmentItem = {
   subtitle: string
   description: string
   image: string
+  color: string
 }
 
 const DepartmentData: DepartmentItem[] = [
@@ -12,31 +13,36 @@ const DepartmentData: DepartmentItem[] = [
     title: "Sjöhumlor - ",
     subtitle: "Spårare 8-9 år",
     description: "vid 8 års ålder kan man börja i HSS, dvs när man börjar i årskurs 2. Man blir då Spårarscout och medlem i avdelningen Sjöhumlorna. (Tidigare hette spårarscout ”miniorscout”) Vi har möten varje vecka samt övernattningar, både hajker och längre läger",
-    image: "/images/department1.png"
+    image: "/images/department1.png",
+    color: "green"
   },
   {
     title: "Kaparna - ",
     subtitle: "Spårare 10-11 år",
     description: "På Kaparna börjar man vid 10 års ålder och stannar kvar i två år tills det är dags för nästa steg. (Tidigare hette upptäckarscout ”juniorscout”) På Kaparna stegras svårighetsgraden på uppgifterna.",
-    image: "/images/department2.png"
+    image: "/images/department2.png",
+    color: "blue"
   },
   {
     title: "Konvojen - ",
     subtitle: " Äventyrare 12-14 år",
     description: "Vid 12 års ålder öppnas dörrarna till Äventyrarscouterna. I tre spännande år får man lära sig nyttiga saker inom sjömansskap och natur. (Tidigare hette äventyrarscout ”patrullscout”)",
-    image: "/images/department3.png"
+    image: "/images/department3.png",
+    color: "orange",
   },
   {
     title: "Utmanare - ",
     subtitle: "15+ år",
     description: "HSS utmanarscoutlag upphör aldrig att existera! Efter tiden som utmanarscout är man ett tätt sammansvetsat gäng. Det är inte ovanligt att man håller kontakten livet ut. (Tidigare hette denna avdelning ”seniorscout”)",
-    image: "/images/department4.png"
+    image: "/images/department4.png",
+    color: "pink"
   },
   // {
   //   title: "",
   //   subtitle: "",
   //   description: "",
-  //   image: "/images/department5.png"
+  //   image: "/images/department5.png",
+  //   color: ""
   // }
 ] 
 
