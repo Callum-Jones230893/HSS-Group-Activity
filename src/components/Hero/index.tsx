66e1b430a493120334e2e999b4076275cc5f15ea
claudiacarion <claudia.carion@gmail.com>
2026-05-25 14:37:06 +0200

    enlarge margin above title

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -18,7 +18,7 @@ const Hero = ({ subtitle, title }: HeroProps) => {
       min-h-213
       md:bg-center
       md:min-h-241.25">
-      <div className="flex flex-col items-center justify-center mt-8 w-65.75 md:mt-24 md:w-188 mx-auto">
+      <div className="flex flex-col items-center justify-center mt-8 w-65.75 md:mt-[160px] md:w-188 mx-auto">
         <h3 className="text-secondary font-primary md:text-[28px]">{subtitle}</h3>
         <h1 className="text-white font-secondary text-[22px] md:text-[40px] mt-4 text-center">{title}</h1>
         <div className="flex flex-col font-primary gap-4 my-4 md:flex-row md:my-8 md:gap-8">
