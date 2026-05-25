f77bf43551164e1271d066e705495af80f32ea24
Callum Jones <call_jones2@hotmail.com>
2026-05-25 11:22:09 +0200

    updated to swedish titles

--- src/components/ScoutPage/index.tsx
+++ src/components/ScoutPage/index.tsx
@@ -10,7 +10,7 @@ type CallToAction = {
 }
 
 const scout: CallToAction = {
-  title: "Become a member",
+  title: "Bli scout",
   subtitle: "OF OUR ADVENTURE GROUP OF FRIENDS",
   description: "I Scouterna hittar du både utmaningar och nya kompisar, oavsett var du bor eller hur du gammal du är. Här kan du uppleva äventyr i naturen, upptäcka nya sätt att lösa problem eller bara ha kul tillsammans medan vi utforskar världen. Och du är självklart med och bestämmer vad vi ska hitta på.",
   image: "/images/001.jpg",
@@ -19,7 +19,7 @@ const scout: CallToAction = {
 }
 
 const leader: CallToAction = {
-  title: "Become a leader",
+  title: "Bli en ledare",
   subtitle: "IN SCOUTING TROUPE WITH POWER",
   description: "Som vuxen finns det utmärkta möjligheter att utvecklas som ledare hos oss. Du behöver inte vara en erfaren seglare för att delta i våra aktiviteter. Du behöver inte heller vara en gammal scout för att bli ledare. Det enda kravet är att du har ett genuint intresse för att arbeta med unga människor och att du är intresserad av att arbeta ideellt i en välfungerande scoutkår. Kåren bekostar den utbildning du behöver för att fungera bra som scoutledare.",
   image: "/images/002.png",
@@ -28,7 +28,7 @@ const leader: CallToAction = {
 }
 
 const cabin: CallToAction = {
-  title: "Rent a cozy cabin",
+  title: "Hyr en mysig stuga",
   subtitle: "TO EXPLORE THE FREEDOM OF NATURE",
   description: "I Scouterna hittar du både utmaningar och nya kompisar, oavsett var du bor eller hur du gammal du är. Här kan du uppleva äventyr i naturen, upptäcka nya sätt att lösa problem eller bara ha kul tillsammans medan vi utforskar världen. Och du är självklart med och bestämmer vad vi ska hitta på.",
   image: "/images/003.png",
@@ -37,7 +37,7 @@ const cabin: CallToAction = {
 }
 
 const partner: CallToAction = {
-  title: "Become a partner",
+  title: "Bli partner",
   subtitle: "WITH OUR PRIDE OF NEXT LEADERS",
   description: "Som vuxen finns det utmärkta möjligheter att utvecklas som ledare hos oss. Du behöver inte vara en erfaren seglare för att delta i våra aktiviteter. Du behöver inte heller vara en gammal scout för att bli ledare. Det enda kravet är att du har ett genuint intresse för att arbeta med unga människor och att du är intresserad av att arbeta ideellt i en välfungerande scoutkår. Kåren bekostar den utbildning du behöver för att fungera bra som scoutledare.",
   image: "/images/004.png",
