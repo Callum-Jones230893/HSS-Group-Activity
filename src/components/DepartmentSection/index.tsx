725c1da38c9ad9d4810e90d81f8bc9bf846c8b07
Oscar Öjling <oscarojling@gmail.com>
2026-05-25 12:02:00 +0200

    Changed md to lg

--- src/components/DepartmentSection/index.tsx
+++ src/components/DepartmentSection/index.tsx
@@ -29,12 +29,12 @@ const DepartmentPage = ({
   return (
     <section className="w-full font-primary">
       <div className="bg-primary">
-        <div className="max-w-432 mx-auto px-4 md:px-36.75 pt-6 pb-10 md:py-20 flex flex-col md:flex-row gap-2 md:items-center">
-          <div className="flex-1 flex flex-col gap-6 text-white md:max-w-175">
-            <h1 className="text-[32px] md:text-[53px]">
+        <div className="max-w-432 mx-auto px-4 lg:px-36.75 pt-6 pb-10 lg:py-20 flex flex-col lg:flex-row gap-2 lg:items-center">
+          <div className="flex-1 flex flex-col gap-6 text-white lg:max-w-175">
+            <h1 className="text-[32px] lg:text-[53px]">
               Hässelby Strands Sjöscoutkår
             </h1>
-            <p className="text-[18px] font-light md:text-[24px]">{intro}</p>
+            <p className="text-[18px] font-light lg:text-[24px]">{intro}</p>
             <button className="w-fit p-4 bg-secondary text-primary rounded-full">
               Become a scout Placeholder
             </button>
@@ -44,28 +44,28 @@ const DepartmentPage = ({
             alt={departmentName}
             width={471}
             height={471}
-            className="hidden md:block object-cover w-117.75 h-117.75 rounded-2xl"
+            className="hidden lg:block object-cover w-117.75 h-117.75 rounded-2xl"
           />
         </div>
       </div>
 
       <div className="bg-primary/5">
-        <div className="max-w-432 mx-auto px-4 md:px-36.75 py-10 md:py-20">
-          <div className="bg-white rounded-2xl p-4 flex flex-col md:flex-row gap-4 md:gap-2.5 max-w-347.75 mx-auto">
+        <div className="max-w-432 mx-auto px-4 lg:px-36.75 py-10 lg:py-20">
+          <div className="bg-white rounded-2xl p-4 flex flex-col lg:flex-row gap-4 lg:gap-2.5 max-w-347.75 mx-auto">
             <Image
               src={icon}
               alt={departmentName}
               width={244}
               height={244}
-              className="object-contain w-29.5 h-29.5 md:h-56 md:w-56"
+              className="object-contain w-29.5 h-29.5 lg:h-56 lg:w-56"
             />
 
-            <div className="flex-1 flex flex-col md:max-w-225">
-              <h2 className="text-primary font-extralight text-[20px] md:text-[24px] mb-4">
+            <div className="flex-1 flex flex-col lg:max-w-225">
+              <h2 className="text-primary font-extralight text-[20px] lg:text-[24px] mb-4">
                 {departmentName} - {age}
               </h2>
 
-              <p className="text-[18px] font-light md:text-[24px] mb-8">
+              <p className="text-[18px] font-light lg:text-[24px] mb-8">
                 {description}
               </p>
 
