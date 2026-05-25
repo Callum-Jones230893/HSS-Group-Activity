e348c72f5f0d8a3c56e3bcacf159c11fce2cdd3e
Callum Jones <call_jones2@hotmail.com>
2026-05-25 16:11:41 +0200

    removed styling typo

--- src/components/DepartmentSelection/index.tsx
+++ src/components/DepartmentSelection/index.tsx
@@ -67,7 +67,7 @@ const DepartmentData: DepartmentItem[] = [
 const DepartmentSelection = () => {
   return (
     <section className="my-22">
-      <div className="flex flex-col w-9/10 lg:w-7/10 mx-auto text-pri">
+      <div className="flex flex-col w-9/10 lg:w-7/10 mx-auto">
         {DepartmentData.map((item, index) => 
           <DepartmentCards key={index} item={item} />
         )}
