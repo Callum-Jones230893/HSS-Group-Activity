df64dfd1d74d64f3da49cdc1269b05d1a66b300f
claudiacarion <claudia.carion@gmail.com>
2026-05-25 15:19:19 +0200

    fix hero sizing to fit on landing

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -10,15 +10,14 @@ type HeroProps = {
 const Hero = ({ subtitle, title }: HeroProps) => {
   return (
     <div
-      className="flex flex-col items-center h-screen justify-between
+      className="flex flex-col items-center justify-between
       bg-[url('/images/hero_background.png')]
       bg-no-repeat
       bg-cover
       bg-position-[-120px_center]
-      min-h-213
       md:bg-center
-      md:min-h-241.25">
-      <div className="flex flex-col items-center justify-center mt-8 w-65.75 md:mt-40 md:w-188 mx-auto">
+      md:min-h-175">
+      <div className="flex flex-col items-center justify-center mt-8 w-65.75 md:mt-15 md:w-188 mx-auto">
         <h3 className="text-secondary font-primary md:text-[28px]">{subtitle}</h3>
         <h1 className="text-white font-secondary text-[22px] md:text-[40px] mt-4 text-center">{title}</h1>
         <div className="flex flex-col font-primary gap-4 my-4 md:flex-row md:my-8 md:gap-8">
@@ -33,7 +32,7 @@ const Hero = ({ subtitle, title }: HeroProps) => {
       <Image
         src="/images/hss_brand.png"
         alt="HSS branding"
-        width={457}
+        width={380}
         height={109}
         className="opacity-65 hidden md:block pb-6"
       />
