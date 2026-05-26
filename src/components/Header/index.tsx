ba148cc2bec856403fbda1a0e36e493e41bfc7d8
Oscar Öjling <oscarojling@gmail.com>
2026-05-26 12:26:17 +0200

    Added link to scouterna in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className=" bg-white flex items-center justify-center lg:w-5/6 lg:mx-auto">
+    <header className=" bg-white flex items-center justify-center lg:w-5/6 lg:mx-auto pt-12 lg:pt-0">
       <Link href="/" className="shrink-0 p-4">
         <Image
           src="/images/hss_logo.png"
@@ -16,6 +16,19 @@ const Header = () => {
         />
       </Link>
       <Navigation />
+      <a
+        href="https://www.scouterna.se/"
+        target="_blank"
+        className="flex items-center gap-2.5 p-2.5 bg-[#EAEAEA] rounded-bl-[10px] absolute top-0 right-0"
+      >
+        <Image
+          src="/images/scouterna_logo.png"
+          alt="scouterna logo"
+          width={105}
+          height={23}
+          className="w-22.5 h-4.75 lg:w-26.25 lg:h-5.75"
+        />
+      </a>
     </header>
   );
 };
