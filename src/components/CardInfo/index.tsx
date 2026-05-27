483492e9f5bef1e651437c70c1165fe3408de23f
Nathalie <nathaliebuss21@gmail.com>
2026-05-27 13:33:50 +0200

    fixed responsivness for multiple devices

--- src/components/CardInfo/index.tsx
+++ src/components/CardInfo/index.tsx
@@ -11,16 +11,16 @@ const CardInfo = ({ title, date, description }: CardInfoProps) => {
     
     return (
         <>  
-        <div className=" flex lg:flex-row lg:p-[16] lg:gap-[1] lg:rounded-[8] lg:w-8/10 md:gap-[28]">
-            <div className="flex flex-row md:justify-center md:m-auto bg-[#ffffff] font-primary rounded-xl w-[326]">
-                <div className="lg:flex lg:grow lg:flex-col flex-col grow font-primary rounded-xl  w-full max-w-432  ">
+        <div className=" flex lg:flex-row lg:p-4 lg:gap-[1] lg:rounded-[8] lg:w-full md:w-[326] md:gap-[28]">
+            <div className="max-w-75 md:flex flex-row lg:max-w-9/10 md:justify-center md:m-auto bg-[#ffffff] font-primary rounded-xl ">
+                <section className="lg:flex lg:grow lg:flex-col flex-col grow font-primary rounded-xl w-full max-w-432  ">
                     <Image
                         src="/images/006.png"
                         alt="grey haired woman with her back towards the camera and a blonde looking at her"
                         width={326}
                         height={217}
                         loading="eager"
-                        className="flex lg:flex-col rounded-t-xl"
+                        className="flex lg:flex-col lg:w-[652] rounded-t-xl"
                     />
                     <div className="flex-col lg:flex-col pt-[16] pl-[16] pr-[16] w-full">
                         <h2 className="font-semibold text-primary py-[7]">
@@ -32,20 +32,20 @@ const CardInfo = ({ title, date, description }: CardInfoProps) => {
                         <p className="flex lg:grow lg:overflow-order text-[#5c5c5c] text-md w-full">
                             {description}
                         </p>
-                        <div className="flex h-[83] py-[24] gap-[13]">
-                            <p className=" bg-amber-300  h-[35] rounded-full py-[7] px-[25] text-primary text-center w-[136]">
+                        <div className="flex lg:flex-start lg:flex-row py-[24] w-full gap-[13]">
+                            <p className=" bg-secondary h-[35] rounded-full py-[7] px-[25] text-primary text-center w-[136]">
                                 Ansök här
                             </p>{" "}
                             {/* button here */}
                             <Link
                                 href={""}
-                                className="decoration-1 px-[10] py-[7] text-primary text-center underline underline-offset-2"
+                                className="flex lg:flex-start decoration-1 px-[10] py-[7] text-primary text-center underline underline-offset-2"
                             >
                                 Read More
                             </Link>
                         </div>
                     </div>
-                </div>
+                </section>
             </div>
         </div>
         </>
