cd9cf3954939e3b8a4f939139692f95a64cd61bd
Nathalie <nathaliebuss21@gmail.com>
2026-05-27 14:16:25 +0200

    fixing indentation

--- src/components/CardInfo/index.tsx
+++ src/components/CardInfo/index.tsx
@@ -2,54 +2,49 @@ import Link from "next/link";
 import Image from "next/image";
 
 type CardInfoProps = {
-    title: string;
-    date: string;
-    description: string;
+  title: string;
+  date: string;
+  description: string;
 };
 
 const CardInfo = ({ title, date, description }: CardInfoProps) => {
-    
-    return (
-        <>  
-        <div className=" flex lg:flex-row lg:p-4 lg:gap-[1] lg:rounded-[8] lg:w-full md:w-[326] md:gap-[28]">
-            <div className="max-w-75 md:flex flex-row lg:max-w-9/10 md:justify-center md:m-auto bg-[#ffffff] font-primary rounded-xl ">
-                <section className="lg:flex lg:grow lg:flex-col flex-col grow font-primary rounded-xl w-full max-w-432  ">
-                    <Image
-                        src="/images/006.png"
-                        alt="grey haired woman with her back towards the camera and a blonde looking at her"
-                        width={326}
-                        height={217}
-                        loading="eager"
-                        className="flex lg:flex-col lg:w-[652] rounded-t-xl"
-                    />
-                    <div className="flex-col lg:flex-col pt-[16] pl-[16] pr-[16] w-full">
-                        <h2 className="font-semibold text-primary py-[7]">
-                            {title}
-                        </h2>
-                        <p className="font-medium py-[7] text-primary">
-                            {date}
-                        </p>
-                        <p className="flex lg:grow lg:overflow-order text-[#5c5c5c] text-md w-full">
-                            {description}
-                        </p>
-                        <div className="flex lg:flex-start lg:flex-row py-[24] w-full gap-[13]">
-                            <p className=" bg-secondary h-[35] rounded-full py-[7] px-[25] text-primary text-center w-[136]">
-                                Ansök här
-                            </p>{" "}
-                            {/* button here */}
-                            <Link
-                                href={""}
-                                className="flex lg:flex-start decoration-1 px-[10] py-[7] text-primary text-center underline underline-offset-2"
-                            >
-                                Read More
-                            </Link>
-                        </div>
-                    </div>
-                </section>
+  return (
+    <>
+      <div className=" flex lg:flex-row lg:p-4 lg:gap-[1] lg:rounded-[8] lg:w-full md:w-[326] md:gap-[28]">
+        <div className="max-w-75 md:flex flex-row lg:max-w-9/10 md:justify-center md:m-auto bg-[#ffffff] font-primary rounded-xl ">
+          <section className="lg:flex lg:grow lg:flex-col flex-col grow font-primary rounded-xl w-full max-w-432  ">
+            <Image
+              src="/images/006.png"
+              alt="grey haired woman with her back towards the camera and a blonde looking at her"
+              width={326}
+              height={217}
+              loading="eager"
+              className="flex lg:flex-col lg:w-[652] rounded-t-xl"
+            />
+            <div className="flex-col lg:flex-col pt-[16] pl-[16] pr-[16] w-full">
+              <h2 className="font-semibold text-primary py-[7]">{title}</h2>
+              <p className="font-medium py-[7] text-primary">{date}</p>
+              <p className="flex lg:grow lg:overflow-order text-[#5c5c5c] text-md w-full">
+                {description}
+              </p>
+              <div className="flex lg:flex-start lg:flex-row py-[24] w-full gap-[13]">
+                <p className=" bg-secondary h-[35] rounded-full py-[7] px-[25] text-primary text-center w-[136]">
+                  Ansök här
+                </p>
+                {/* button here */}
+                <Link
+                  href={""}
+                  className="flex lg:flex-start decoration-1 px-[10] py-[7] text-primartext-center underline underline-offset-2"
+                >
+                  Read More
+                </Link>
+              </div>
             </div>
+          </section>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default CardInfo;
--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,57 +1,59 @@
 import CardInfo from "../CardInfo";
 
 type Test = {
-    title: string;
-    date: string;
-    description: string;
-    image: string;
+  title: string;
+  date: string;
+  description: string;
+  image: string;
 };
 
-const data:Test[] = [
-    {
-        title: "Håll ut 2026 storvallens fjällstugor, Storlien",
-        date: "Datum: 5-20 Juli 2026",
-        description:
-        "Under arrangemanget kommer fjällen var vår bas och en stor del av arrangemanget kommer vara utomhus...",
-        image: "../../../public/images/006.png",
-    },
-    {
-        title: "Håll ut 2026 storvallens fjällstugor, Storlien",
-        date: "Datum: 5-20 Juli 2026",
-        description:
-        "Under arrangemanget kommer fjällen var vår bas och en stor del av arrangemanget kommer vara utomhus...",
-        image: "../../../public/images/006.png",
-    },
-    {
-        title: "Håll ut 2026 storvallens fjällstugor, Storlien",
-        date: "Datum: 5-20 Juli 2026",
-        description:
-        "Under arrangemanget kommer fjällen var vår bas och en stor del av arrangemanget kommer vara utomhus...",
-        image: "../../../public/images/006.png",
-    },               
+const data: Test[] = [
+  {
+    title: "Håll ut 2026 storvallens fjällstugor, Storlien",
+    date: "Datum: 5-20 Juli 2026",
+    description:
+      "Under arrangemanget kommer fjällen var vår bas och en stor del av arrangemanget kommer vara utomhus...",
+    image: "../../../public/images/006.png",
+  },
+  {
+    title: "Håll ut 2026 storvallens fjällstugor, Storlien",
+    date: "Datum: 5-20 Juli 2026",
+    description:
+      "Under arrangemanget kommer fjällen var vår bas och en stor del av arrangemanget kommer vara utomhus...",
+    image: "../../../public/images/006.png",
+  },
+  {
+    title: "Håll ut 2026 storvallens fjällstugor, Storlien",
+    date: "Datum: 5-20 Juli 2026",
+    description:
+      "Under arrangemanget kommer fjällen var vår bas och en stor del av arrangemanget kommer vara utomhus...",
+    image: "../../../public/images/006.png",
+  },
 ];
 
 const Cards = () => {
-    const maxChar = (description: string) =>
-        description.substring(0, 105) + (description.length > 105 ? "..." : "");
+  const maxChar = (description: string) =>
+    description.substring(0, 105) + (description.length > 105 ? "..." : "");
 
-    return (
-        <>
-            <div className="flex flex-col grow justify-center bg-[#e8eaee] rounded-[12] m-6 lg:pb-[42] lg:pt-[42] lg:px-[32] lg:py-[42] p-7.5 lg:m-[40]">
-                <h2 className="text-[#053a60] max-w-[1452] font-primary text-4xl text-light text-center lg:text-left lg:pl-8 pt-2 mb-3 md:text-center ">Upcoming events on HSS</h2>
-                    <div className="grid lg:grid-cols-3 items-center gap-y-7 gap-x-2  justify-center md:margin-auto">
-                        {data.map((dataItem, index) => (
-                            <CardInfo
-                            key={index}
-                            title={dataItem.title}
-                            date={dataItem.date}
-                            description={maxChar(dataItem.description)}
-                            />
-                        ))}
-                    </div>
-                </div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex flex-col grow justify-center bg-[#e8eaee] rounded-[12] m-6 lg:pb-[42] lg:pt-[42] lg:px-[32] lg:py-[42] p-7.5 lg:m-[40]">
+        <h2 className="text-[#053a60] max-w-[1452] font-primary text-4xl text-light text-center lg:text-left lg:pl-8 pt-2 mb-3 md:text-center ">
+          Upcoming events on HSS
+        </h2>
+        <div className="grid lg:grid-cols-3 items-center gap-y-7 gap-x-2  justify-center md:margin-auto">
+          {data.map((dataItem, index) => (
+            <CardInfo
+              key={index}
+              title={dataItem.title}
+              date={dataItem.date}
+              description={maxChar(dataItem.description)}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Cards;
