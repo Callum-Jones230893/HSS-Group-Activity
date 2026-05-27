15eea6df9134cebd317720fdae39c823a150e44f
claudiacarion <claudia.carion@gmail.com>
2026-05-27 17:02:18 +0200

    add stying for scouterna link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className=" bg-white flex items-center justify-center z-999 sticky top-0 md:py-4 lg:p-11.25 lg:w-5/6 lg:mx-auto">
+    <header className=" bg-white flex items-center justify-center z-999 sticky top-0 md:py-4 lg:w-full lg:mx-auto pt-10 lg:pt-4">
       <Link href="/" className="shrink-0 pl-4">
         <Image
           src="/images/hss_logo.png"
@@ -12,7 +12,7 @@ const Header = () => {
           width={68}
           height={68}
           loading="eager"
-          className="w-13 h-13 md:w-17 md:h-17"
+          className="w-13 h-13 md:w-17 md:h-17 lg:ml-30"
         />
       </Link>
       <Navigation />
