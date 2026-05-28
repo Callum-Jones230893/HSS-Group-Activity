bba75ee860e244a981d4596d39e6403637eaab54
claudiacarion <claudia.carion@gmail.com>
2026-05-28 10:30:20 +0200

    add more button

--- src/components/TestingHoverEffects/index.tsx
+++ src/components/TestingHoverEffects/index.tsx
@@ -2,7 +2,7 @@ const TestingHoverEffects = () => {
   return (
     <div className='flex flex-col w-full items-center justify-center gap-4 p-10'>
       <button className="w-fit text-primary bg-secondary h-[53px] px-[29px] py-[12px] rounded-[30px] hover:bg-[#e5a000] cursor-pointer">color change</button>
-      <button className="w-fit text-primary bg-secondary h-[53px] px-[29px] py-[12px] rounded-[30px] hover:bg-[#e5a000] cursor-pointer">color change</button>
+      <button className="w-fit border-secondary border border-solid h-[53px] px-[29px] py-[12px] rounded-[30px] hover:bg-[#e5a000] cursor-pointer">color change</button>
     </div>
   );
 };
