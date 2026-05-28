4f9c17c464ecb32b108721b8a6ae8003a887e08c
Nathalie <nathaliebuss21@gmail.com>
2026-05-28 18:13:10 +0200

    responsive section

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -54,12 +54,12 @@ const data: Test[] = [
 const Cards = () => {
   return (
     <>
-    <section className="w-full max-w-[1728] flex flex-col justify-center px-1 lg:px-[151] p-7.5 md:m-auto  lg:max-w-19/20">
-      <div className="flex flex-col lg:mx-auto justify-center items-center bg-[#0036600F] rounded-[12] m-3.75 lg:pb-[42] lg:pt-[42] lg:py-[74] p-7.5 items-stretch ">
+    <section className="w-full max-w-[1728] lg:mx-auto flex flex-col items-stretch px-4 lg:py-18.5 lg:px-[151px]lg:px-12 mt-3.75 md:m-auto py-10 lg:max-w-168/200 overflow-none">
+      <div className="flex flex-col  bg-[#0036600F] rounded-[12] m-3.75 lg:pb-[42] lg:pt-[42] lg:py-[74] p-7.5">
         <h2 className="text-[#053a60] max-w-[1452] font-primary md:text-4xl text-[26px] text-light text-center lg:text-left lg:pl-8 pt-2 pb-5 md:text-center ">
           Tidigare Event Inom HSS
         </h2>
-        <div className="grid xl:grid-cols-3 items-center lg:gap-3 gap-y-7 gap-x-2 justify-center md:grid-cols-2 md:margin-auto">
+        <div className="grid xl:grid-cols-3 items-center lg:gap-3 gap-y-7 gap-x-9 md:gap-x-3md:gap-y-2 justify-center md:grid-cols-2 md:margin-auto">
           {data.map((dataItem, index) => (
             <CardInfo
               key={index}
