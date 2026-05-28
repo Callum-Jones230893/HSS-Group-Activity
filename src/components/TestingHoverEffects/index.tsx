ada7ab89fae46a83c42f500efedbe6293be28092
claudiacarion <claudia.carion@gmail.com>
2026-05-28 12:00:18 +0200

    add more buttons

--- src/components/TestingHoverEffects/index.tsx
+++ src/components/TestingHoverEffects/index.tsx
@@ -1,8 +1,34 @@
 const TestingHoverEffects = () => {
   return (
-    <div className='flex flex-col w-full items-center justify-center gap-4 p-10'>
-      <button className="w-fit text-primary bg-secondary h-[53px] px-[29px] py-[12px] rounded-[30px] hover:bg-[#e5a000] cursor-pointer">color change</button>
-      <button className="w-fit border-secondary border border-solid h-[53px] px-[29px] py-[12px] rounded-[30px] hover:bg-[#e5a000] cursor-pointer">color change</button>
+    <div className="flex flex-col w-full items-center justify-center gap-24 p-10">
+      <div className="flex gap-4">
+        <button className="w-fit text-primary bg-secondary h-[53px] px-[29px] py-[12px] rounded-[30px] hover:bg-[#e5a000] cursor-pointer">
+          color change
+        </button>
+        <button className="w-fit text-primary bg-secondary h-[53px] px-[29px] py-[12px] rounded-[30px] hover:bg-primary hover:text-secondary cursor-pointer">
+          color change
+        </button>
+        <button className="w-fit text-primary border-secondary border border-solid h-[53px] px-[29px] py-[12px] rounded-[30px] hover:bg-secondary cursor-pointer">
+          color change
+        </button>
+        <button className="w-fit text-primary border-secondary border border-solid h-[53px] px-[29px] py-[12px] rounded-[30px] hover:bg-primary hover:text-secondary hover:border-primary cursor-pointer">
+          color change
+        </button>
+      </div>
+      <div className="flex gap-4">
+        <button className="w-fit text-primary bg-secondary h-[53px] px-[29px] py-[12px] rounded-[30px] cursor-pointer">
+          scale
+        </button>
+        <button className="w-fit text-primary bg-secondary h-[53px] px-[29px] py-[12px] rounded-[30px]  cursor-pointer">
+          scale
+        </button>
+        <button className="w-fit text-primary border-secondary border border-solid h-[53px] px-[29px] py-[12px] rounded-[30px]  cursor-pointer">
+          scale
+        </button>
+        <button className="w-fit text-primary border-secondary border border-solid h-[53px] px-[29px] py-[12px] rounded-[30px]  cursor-pointer">
+          scale
+        </button>
+      </div>
     </div>
   );
 };
