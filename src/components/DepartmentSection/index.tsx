0b091fd4a9c291eed1d27918dfdcf0ae53014f93
Oscar Öjling <oscarojling@gmail.com>
2026-05-29 22:54:15 +0200

    Imported deparmentselection for easier integration, changed props names

--- src/components/DepartmentSection/index.tsx
+++ src/components/DepartmentSection/index.tsx
@@ -1,72 +1,48 @@
 import Image from "next/image";
 
-type Leader = {
-  name: string;
-  role: string;
-  email: string;
-  avatar: string;
-};
-
-type DepartmentPageProps = {
-  departmentName: string;
-  age: string;
-  icon: string;
+type DepartmentSectionProps = {
+  title: string;
+  subtitle: string;
+  image: string;
   description: string;
-  leader: Leader;
+  text: string;
 };
 
-const DepartmentPage = ({
-  departmentName,
-  age,
-  icon,
+const DepartmentSection = ({
+  title,
+  subtitle,
+  image,
   description,
-  leader,
-}: DepartmentPageProps) => {
+  text,
+}: DepartmentSectionProps) => {
   return (
     <section className="w-full font-primary">
       <div className="bg-primary/5">
-        <div className="max-w-432 mx-auto px-4 lg:px-36.75 py-10 lg:py-20">
-          <div className="bg-white rounded-2xl p-4 flex flex-col lg:flex-row gap-4 lg:gap-2.5 max-w-347.75 mx-auto">
+        <div className="max-w-432 mx-auto px-4 md:px-36.75 py-10 md:py-20">
+          <div className="bg-white rounded-2xl p-4 flex flex-col md:flex-row gap-4 md:gap-2.5 max-w-347.75 mx-auto">
             <Image
-              src={icon}
-              alt={departmentName}
+              src={image}
+              alt={title}
               width={244}
               height={244}
-              className="object-contain w-29.5 h-29.5 lg:h-56 lg:w-56"
+              className="object-contain w-29.5 h-29.5 md:h-56 md:w-56"
             />
 
-            <div className="flex-1 flex flex-col lg:max-w-225">
-              <h2 className="text-primary font-extralight text-[20px] lg:text-[24px] mb-4">
-                {departmentName} - {age}
-              </h2>
+            <div className="flex-1 flex flex-col md:max-w-225">
+              <div className="flex items-center gap-1.5 mb-4">
+                <h2 className="text-primary font-extralight text-[20px] md:text-[24px]">
+                  {title}
+                </h2>
+                <p
+                  className={`font-extralight text-[20px] md:text-[24px] ${text}`}
+                >
+                  {subtitle}
+                </p>
+              </div>
 
-              <p className="text-[18px] font-light lg:text-[24px] mb-8">
+              <p className="text-[18px] font-light md:text-[24px]">
                 {description}
               </p>
-
-              <p className="text-[20px] font-light mb-3">Avdelningsledare:</p>
-              <div className="flex items-center gap-4">
-                <Image
-                  src={leader.avatar}
-                  alt={leader.name}
-                  width={76}
-                  height={76}
-                  className="rounded-full object-cover"
-                />
-
-                <div className="flex flex-col gap-1">
-                  <p className="text-[20px]">{leader.name}</p>
-                  <span className="w-fit px-3 py-1 bg-[#3A9B6DB5] text-[#F9F9F9] text-[16px] font-extralight rounded-full">
-                    {leader.role}
-                  </span>
-                  <a
-                    href={`mailto:${leader.email}`}
-                    className="text-[16px] font-light text-[#0290FF]"
-                  >
-                    {leader.email}
-                  </a>
-                </div>
-              </div>
             </div>
           </div>
         </div>
@@ -75,4 +51,4 @@ const DepartmentPage = ({
   );
 };
 
-export default DepartmentPage;
+export default DepartmentSection;
