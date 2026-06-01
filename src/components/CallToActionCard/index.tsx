e7c901de11e83ab0a1e4f8501efb08b1a1984799
Callum Jones <call_jones2@hotmail.com>
2026-06-01 14:34:02 +0200

    updated positioning of images, and spacing between title/subtitle

--- src/components/CallToActionCard/index.tsx
+++ src/components/CallToActionCard/index.tsx
@@ -8,7 +8,7 @@ type CallToAction = {
   description: string
   image: string
   button: string
-  link: string
+  readMore: string
 }
 
 type CallToActionProps = {
@@ -21,19 +21,21 @@ const CallToActionCard = ({item}: CallToActionProps) => {
         xl:mx-14.75 xl:mt-5 lg:justify-center xl:even:flex-row-reverse xl:gap-28"
     >
       <div className="flex flex-col min-h-0 xl:w-163.25 gap-4">
-        <h4 className="text-[24px] font-semibold lg:text-[36px]">{item.title}</h4>
-        <h2 className="text-black text-[28px] lg:text-[44px]">{item.subtitle}</h2>
+        <div className="flex flex-col">
+          <h4 className="text-[24px] font-semibold lg:text-[36px]">{item.title}</h4>
+          <h2 className="text-black text-[28px] lg:text-[44px]">{item.subtitle}</h2>
+        </div>
         <p className=" text-[18px] tracking-[1%] lg:py-15.25 lg:text-[24px]">{item.description}</p>
         <div className="flex gap-10 items-center md:pb-3 text-[16px] lg:text-[18px]">
           <Button content={item.button} theme="yellow" size="large" />
           <div>
-            <Link href="" className="w-37 h-16.25">{item.link}</Link>
+            <Link href="" className="w-37 h-16.25">{item.readMore}</Link>
           </div>
         </div>
       </div>
-      <div className="flex justify-center mx-auto mt-10 lg:mx-auto w-full lg:w-7/10 xl:w-4/10 lg:min-w-150 max-w-150 aspect-4/3">
+      <div className="flex justify-center mx-auto mt-10 lg:my-auto lg:mx-auto w-full lg:w-7/10 xl:w-4/10 lg:min-w-150 max-w-150 aspect-4/3">
         <Image src={item.image} alt={item.title} width="400" height="300" 
-          className="w-full max-h-137.5 object-cover rounded-lg"
+          className="w-full max-h-137.5 xl:h-137.5 object-cover rounded-lg"
         />
       </div>
     </div>
