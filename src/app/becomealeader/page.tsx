6d8b12064a5dcfcaf285342b23ea5596804f0713
Nathalie <nathaliebuss21@gmail.com>
2026-06-01 15:55:17 +0200

    made the become scout leader page

--- src/app/becomealeader/page.tsx
+++ src/app/becomealeader/page.tsx
@@ -1,23 +1,41 @@
 import BecomeALeaderData from "../../data/becomeleader";
 import Button from '@/components/Button'
 
+
 const BecomeALeader = () => {
   const data = BecomeALeaderData;
   return (
-    <div className="flex flex-col bg-primary max-w-107.5 max-h-[1763] lg:max-w[1728px] lg:max-h-578.5">
-      <div className="flex flex-start flex-col px-4  lg:px-37.25 lg:max-w-260.75">
+    <div className="flex flex-col bg-primary ">
+      <div className="flex flex-start flex-col px-4  lg:px-14 lg:py-12">
         <h2 className="text-white font-primary text-[32px] lg:text-[53px] pt-6">{data.title}</h2>
-        <h3 className="text-secondary font-primary pb-6 text-[28px] lg:text-[32px]">{data.subtitle}</h3>
+        <h3 className="text-secondary font-primary pb-11 text-[28px] max-w-[901px] lg:text-[32px]">{data.subtitle}</h3>
         <Button content="Bli Ledare" theme="yellow" size="large"/>
       </div>
       <img
-        src={'public/images/memeber-background.jpg'}
+        src={'/images/member_background.png'}
         height={1779}
         width={1152}
         alt="4 people on a white sailboat"
-        className="py-9"
+        className="py-9 lg:w-screen"
       />
-        <p className=" font-primary text-left text-white max-w-99.5 lg:max-w-313.5 text-[18px] lg:text-[24px] leading-[31.5px] lg:leading-10.5 tracking-[0.18px] lg:tracking-[0.24px] pb-10 px-4 lg:px-36.5">{data.description}</p>
+        <p className=" font-primary text-left text-white  lg:max-w-313.5 text-[18px] lg:text-[24px] leading-[31.5px] lg:leading-10.5 tracking-[0.18px] lg:tracking-[0.24px] pb-10 px-4 lg:px-36.5">{data.descriptionOne}
+          <br/>
+          <br/>
+          {data.descriptionTwo}
+          <br/>
+          <br/>
+          {data.descriptionThree}
+          <br/>
+          <br/>
+          {data.descriptionFour}
+          <br/>
+          <br/>
+          {data.descriptionFive}
+          <br/>
+          <br/>
+          {data.descriptionSix}
+        </p>
+        
     </div>
   );
 };
