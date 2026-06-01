704e61e258a42e26480f431096abc14d3e7970ba
Nathalie <nathaliebuss21@gmail.com>
2026-06-01 16:54:18 +0200

    working on the page

--- src/app/becomealeader/page.tsx
+++ src/app/becomealeader/page.tsx
@@ -1,24 +1,24 @@
 import BecomeALeaderData from "../../data/becomeleader";
 import Button from '@/components/Button'
-
+import Image from 'next/image'
 
 const BecomeALeader = () => {
   const data = BecomeALeaderData;
   return (
     <div className="flex flex-col bg-primary ">
-      <div className="flex flex-start flex-col px-4  lg:px-14 lg:py-12">
+      <div className="flex flex-start flex-col px-4  lg:px-36.5 lg:py-12">
         <h2 className="text-white font-primary text-[32px] lg:text-[53px] pt-6">{data.title}</h2>
         <h3 className="text-secondary font-primary pb-11 text-[28px] max-w-[901px] lg:text-[32px]">{data.subtitle}</h3>
         <Button content="Bli Ledare" theme="yellow" size="large"/>
       </div>
-      <img
-        src={'/images/member_background.png'}
+      <Image
+        src={'/images/022.png'}
         height={1779}
         width={1152}
         alt="4 people on a white sailboat"
-        className="py-9 lg:w-screen"
+        className=" py-9 lg:w-screen lg:max-h-[600px] place-self-center object-cover"
       />
-        <p className=" font-primary text-left text-white  lg:max-w-313.5 text-[18px] lg:text-[24px] leading-[31.5px] lg:leading-10.5 tracking-[0.18px] lg:tracking-[0.24px] pb-10 px-4 lg:px-36.5">{data.descriptionOne}
+        <p className=" font-primary text-left text-white text-[18px] lg:text-[24px] leading-[31.5px] lg:leading-10.5 tracking-[0.18px] lg:tracking-[0.24px] pb-10 px-4 lg:px-36.5">{data.descriptionOne}
           <br/>
           <br/>
           {data.descriptionTwo}
