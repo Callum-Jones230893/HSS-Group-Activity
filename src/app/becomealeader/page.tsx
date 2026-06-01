b04bb679274ed41731f04a8cba94a31a3508ca23
Nathalie <nathaliebuss21@gmail.com>
2026-06-01 13:28:57 +0200

    added the data and started styling

--- src/app/becomealeader/page.tsx
+++ src/app/becomealeader/page.tsx
@@ -1,7 +1,25 @@
-const BecomeALeader = () => {
-  return (
-    <p className="text-4xl text-primary">Coming soon...</p>
-  )
-}
+import BecomeALeaderData from "../../data/becomeleader";
+import Button from '@/components/Button'
 
-export default BecomeALeader
+const BecomeALeader = () => {
+  const data = BecomeALeaderData;
+  return (
+    <div className="flex flex-col bg-primary max-w-107.5 max-h-[1763] lg:max-w[1728px] lg:max-h-578.5">
+      <div className="flex flex-start flex-col px-4  lg:px-37.25 lg:max-w-260.75">
+        <h2 className="text-white font-primary text-[32px] lg:text-[53px] pt-6">{data.title}</h2>
+        <h3 className="text-secondary font-primary pb-6 text-[28px] lg:text-[32px]">{data.subtitle}</h3>
+        <Button content="Bli Ledare" theme="yellow" size="large"/>
+      </div>
+      <img
+        src={'public/images/memeber-background.jpg'}
+        height={1779}
+        width={1152}
+        alt="4 people on a white sailboat"
+        className="py-9"
+      />
+        <p className=" font-primary text-left text-white max-w-99.5 lg:max-w-313.5 text-[18px] lg:text-[24px] leading-[31.5px] lg:leading-10.5 tracking-[0.18px] lg:tracking-[0.24px] pb-10 px-4 lg:px-36.5">{data.description}</p>
+    </div>
+  );
+};
+
+export default BecomeALeader;
---
+++ src/data/becomeleader.ts
@@ -0,0 +1,19 @@
+import img from "../../public/images/member_background.png";
+
+const BecomeALeaderData: {
+  title: string;
+  subtitle: string;
+  image: string;
+  description: string;
+} = {
+  title: "Vill du bli en av oss?",
+  subtitle:
+    "Här kanske ditt livs äventyr startar. I HSS finns det spännande utmaningar för stor som liten.",
+  image: "'../../public/images/member_background.png",
+  description: `Är du sugen på scouting och segling? Tveka inte, då ska du anmäla ditt intresse till HSS. Oavsett om du är 8 eller 100 år är du välkommen. För att anmäla dig till kölistan fyller du in en intresseanmälan nedan. 
+  Vi kontaktar dig när det blir en plats ledig. Som vuxen finns det utmärkta möjligheter att utvecklas som ledare hos oss. Ledarsituationen i kåren varierar, nu är vi i akut behov av ledare till Kaparna. Du behöver inte vara en erfaren seglare för att delta i vår verksamhet. Du behöver inte heller vara gammal scout för att bli ledare. 
+  Enda kravet är att du har ett genuint intresse av att jobba med ungdomar och att du är sugen på att arbeta ideellt i en välfungerande scoutkår. Kåren betalar för den utbildning du behöver för att fungera bra som scoutledare. 
+  Vi behöver alltid föräldrar som kan hjälpa till med stort som smått. Utan föräldrar ingen kår, så enkelt är det!”Scouterna har varit min bästa ledarskola. Där kan man bara leda med idéns kraft och förmågan att övertyga.”[Carl-Henrik Svanberg, fd VD för Ericsson, i Expressen 2003-02-07 ”Inom scoutrörelsen finns ingen avbytarbänk.”[Anonym ledare i HSS]Välkommen!`,
+};
+
+export default BecomeALeaderData;
