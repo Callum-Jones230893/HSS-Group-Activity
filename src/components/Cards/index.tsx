89fa95805060f9cd8c5f758b255fc75064c1dcfb
Nathalie <nathaliebuss21@gmail.com>
2026-06-01 10:21:18 +0200

    removed commented out code

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -71,18 +71,3 @@ const Cards = () => {
 };
 
 export default Cards;
-
-// xl:mx-[145px]
-// lg:mx-[80px]  
-// mx-[14px]
-
-{/* <section className="w-full max-w-[1728] lg:mx-auto flex flex-col items-stretch px-4 lg:py-18.5 lg:px-[151px]lg:px-12 mt-3.75 md:m-auto py-10 lg:max-w-168/200 overflow-none">
-<div className="flex flex-col  bg-[#0036600F] rounded-[12] m-3.75 lg:pb-[42] lg:pt-[42] lg:py-[74] p-7.5">
-  <h2 className="text-[#053a60] max-w-[1452] font-primary md:text-4xl text-[26px] text-light text-center lg:text-left lg:pl-8 pt-2 pb-5 md:text-center ">
-    Tidigare Event Inom HSS
-  </h2>
-  <div className="grid xl:grid-cols-3 items-center lg:gap-3 gap-y-7 gap-x-9 md:gap-x-3md:gap-y-2 justify-center md:grid-cols-2 md:margin-auto"> */}
-
-  {/*  <div className="flex lg:flex-row lg:p-4 lg:gap-[1] lg:rounded-[8] md:gap-[28]">
-    <div className="max-w-75 md:flex flex-row  md:justify-center md:m-auto bg-[#ffffff] font-primary rounded-xl ">
-      <section className="lg:flex lg:grow lg:flex-col flex-col grow font-primary rounded-xl w-full max-w-432  "> */}
