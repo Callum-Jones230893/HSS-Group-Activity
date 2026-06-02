9e7d2efb50a6a3d3fe6e52e8f9fe55815cbe33a1
claudiacarion <claudia.carion@gmail.com>
2026-06-02 14:25:39 +0200

    first commit

---
+++ src/components/ContactUs/index.tsx
@@ -0,0 +1,17 @@
+type ContactUsProps = {
+  title: string,
+  subtitle: string,
+  fullName: string,
+  message: string,
+  email: string
+}
+
+const ContactUs = () => {
+  return (
+    <div className="bg-primary">
+      <h2 className='text-secondary'>{}</h2>
+    </div>
+  )
+}
+
+export default ContactUs
