9aa72f88df50caa7f674af0dca0a81e42655e896
Oscar Öjling <oscarojling@gmail.com>
2026-06-02 22:48:13 +0200

    Made image smaller for space on page

--- src/components/SectionHero/index.tsx
+++ src/components/SectionHero/index.tsx
@@ -11,7 +11,7 @@ type HeroProps = {
 const Hero = ({ title, intro, image, button }: HeroProps) => {
   return (
     <div className="bg-primary">
-      <div className="max-w-432 mx-auto px-4 md:px-36.75 pt-6 pb-10 md:py-20 flex flex-col lg:flex-row gap-8 lg:items-center">
+      <div className="max-w-432 mx-auto px-4 md:px-36.75 pt-6 pb-10 md:py-12 lg:py-12 flex flex-col lg:flex-row gap-8 lg:items-center">
         <div className="flex-1 flex flex-col gap-6 text-white">
           <h1 className="text-[32px] md:text-[53px]">{title}</h1>
           <p className="text-[18px] font-light md:text-[24px]">{intro}</p>
@@ -23,7 +23,7 @@ const Hero = ({ title, intro, image, button }: HeroProps) => {
             alt={title}
             width={471}
             height={471}
-            className="hidden md:block object-cover w-117.75 h-117.75 rounded-2xl"
+            className="hidden md:block object-cover w-80 h-80 rounded-2xl"
           />
         )}
       </div>
