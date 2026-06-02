9d331cb80e002ed3bfc4f08d71b6d94d23083776
Callum Jones <call_jones2@hotmail.com>
2026-06-02 12:53:28 +0200

    updated with loading

--- src/components/CallToActionCard/index.tsx
+++ src/components/CallToActionCard/index.tsx
@@ -33,7 +33,7 @@ const CallToActionCard = ({item}: CallToActionProps) => {
         </div>
       </div>
       <div className="flex justify-center mx-auto mt-10 lg:mx-auto w-full lg:w-7/10 xl:w-4/10 max-w-150 aspect-square">
-        <Image src={item.image} alt={item.title} width="400" height="300" 
+        <Image src={item.image} alt={item.title} loading="eager" width="400" height="300" 
           className="w-full max-h-137.5 object-cover rounded-lg"
         />
       </div>
