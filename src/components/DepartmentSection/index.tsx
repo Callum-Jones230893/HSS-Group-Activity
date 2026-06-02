20455773b3530053e958a6988de90b915373edd4
Oscar Öjling <oscarojling@gmail.com>
2026-06-02 15:20:53 +0200

    Started working on department pages, changed hero and deparment-component responsivnes

--- src/components/DepartmentSection/index.tsx
+++ src/components/DepartmentSection/index.tsx
@@ -18,17 +18,17 @@ const DepartmentSection = ({
   return (
     <section className="w-full font-primary">
       <div className="bg-primary/5">
-        <div className="max-w-432 mx-auto px-4 md:px-36.75 py-10 md:py-20">
-          <div className="bg-white rounded-2xl p-4 flex flex-col md:flex-row gap-4 md:gap-2.5 max-w-347.75 mx-auto">
+        <div className="max-w-432 mx-auto px-4 lg:px-36.75 py-10 lg:py-20">
+          <div className="bg-[#fff] rounded-2xl p-4 flex flex-col lg:flex-row gap-4 lg:gap-2.5 max-w-347.75 mx-auto">
             <Image
               src={image}
               alt={title}
               width={244}
               height={244}
-              className="object-contain w-29.5 h-29.5 md:h-56 md:w-56"
+              className="object-contain w-29.5 h-29.5 lg:h-56 lg:w-56"
             />
 
-            <div className="flex-1 flex flex-col md:max-w-225">
+            <div className="flex-1 flex flex-col lg:max-w-225">
               <div className="flex items-center gap-1.5 mb-4">
                 <h2 className="text-primary font-extralight text-[20px] md:text-[24px]">
                   {title}
--- src/data/department.ts
+++ src/data/department.ts
@@ -3,7 +3,7 @@ export type DepartmentItemType = {
   title: string;
   subtitle: string;
   description: string;
-  fullDescription: string;
+  fullDescription: string[];
   image: string;
   text: string;
   border: string;
@@ -18,8 +18,14 @@ export const DepartmentData: DepartmentItemType[] = [
     subtitle: "Spårare 8-9 år",
     description:
       "Vid 8 års ålder kan man börja i HSS, dvs när man börjar i årskurs 2. Man blir då Spårarscout och medlem i avdelningen Sjöhumlorna. (Tidigare hette spårarscout ”miniorscout”) Vi har möten varje vecka samt övernattningar, både hajker och längre läger",
-    fullDescription:
-      "Vid 8 års ålder kan man börja i HSS, dvs när man börjar i årskurs 2. Man blir då Spårarscout och medlem i avdelningen Sjöhumlorna. (Tidigare hette spårarscout ”miniorscout”) Vi har möten varje vecka samt övernattningar, både hajker och längre läger. Här får barnen lära sig knopar, grundläggande naturkunskap och sjömanskap på ett lekfullt sätt. Du lär dig något nytt nästan varje dag i Scouterna, det är alltid roliga saker men ibland är de också väldigt bra att kunna. Som att tälja med kniv eller lägga bandage på någon som gjort sig illa. Till skillnad från hur man gör i skolan läser vi inga böcker om knivar och bandage, vi lär oss genom att göra, genom att prova på allt på riktigt istället. Medan vi gör det samarbetar vi i patruller, små grupper där alla är med och bestämmer. Det är smart, för tillsammans kan man alltid hjälps åt att hitta på ännu roligare saker. Vi seglar optimistjollar och 2-kronor. Sjöhumlorna har möten på måndagar i vår fina lokal Ruffen vid Hässelby Strandbad. När man är scout gör man alltid sitt bästa och det gör inget om det råkar bli fel, därför brukar vi tänka lite på hur allting gått till så att vi blir bättre till nästa gång.",
+    fullDescription: [
+      "Vid 8 års ålder kan man börja i HSS, dvs när man börjar i årskurs 2. Man blir då Spårarscout och medlem i avdelningen Sjöhumlorna. (Tidigare hette spårarscout ”miniorscout”)",
+      "Vi har möten varje vecka samt övernattningar, både hajker och längre läger. Här får barnen lära sig knopar, grundläggande naturkunskap och sjömanskap på ett lekfullt sätt. ",
+      "Du lär dig något nytt nästan varje dag i Scouterna, det är alltid roliga saker men ibland är de också väldigt bra att kunna. Som att tälja med kniv eller lägga bandage på någon som gjort sig illa. Till skillnad från hur man gör i skolan läser vi inga böcker om knivar och bandage, vi lär oss genom att göra, genom att prova på allt på riktigt istället. Medan vi gör det samarbetar vi i patruller, små grupper där alla är med och bestämmer. Det är smart, för tillsammans kan man alltid hjälps åt att hitta på ännu roligare saker.",
+      "Vi seglar optimistjollar och 2-kronor.",
+      "Sjöhumlorna har möten på måndagar i vår fina lokal Ruffen vid Hässelby Strandbad.",
+      "När man är scout gör man alltid sitt bästa och det gör inget om det råkar bli fel, därför brukar vi tänka lite på hur allting gått till så att vi blir bättre till nästa gång.",
+    ],
     image: "/images/department1.png",
     text: "text-lime-600",
     border: "lg:border-lime-600/50",
@@ -32,8 +38,17 @@ export const DepartmentData: DepartmentItemType[] = [
     subtitle: "Spårare 10-11 år",
     description:
       "På Kaparna börjar man vid 10 års ålder och stannar kvar i två år tills det är dags för nästa steg. (Tidigare hette upptäckarscout ”juniorscout”) På Kaparna stegras svårighetsgraden på uppgifterna.",
-    fullDescription:
-      "På Kaparna börjar man vid 10 års ålder och stannar kvar i två år tills det är dags för nästa steg. (Tidigare hette upptäckarscout ”juniorscout”) På Kaparna stegras svårighetsgraden på uppgifterna. Scouterna får lära sig segla optimist samt prova på att segla 2-krona samt kölbåt. En färdigutbildad upptäckarscout kan bland annat hantera kniv, elda och släcka mindre bränder, segla optimist själv samt grundläggande sjukvård och sjösäkerhet. Avdelningen Kaparna har möten på tisdagar i vår lokal Ruffen vid Hässelby Strandbad. När man är scout gör man alltid sitt bästa men det gör inget om det råkar bli fel, därför brukar vi tänka lite på hur allting gått till så att vi blir bättre till nästa gång.",
+    fullDescription: [
+      "På Kaparna börjar man vid 10 års ålder och stannar kvar i två år tills det är dags för nästa steg. (Tidigare hette upptäckarscout ”juniorscout”) På Kaparna stegras svårighetsgraden på uppgifterna.",
+
+      "Scouterna får lära sig segla optimist samt prova på att segla 2-krona samt kölbåt.",
+
+      "En färdigutbildad upptäckarscout kan bland annat hantera kniv, elda och släcka mindre bränder, segla optimist själv samt grundläggande sjukvård och sjösäkerhet.",
+
+      "Avdelningen Kaparna har möten på tisdagar i vår lokal Ruffen vid Hässelby Strandbad.",
+
+      "När man är scout gör man alltid sitt bästa men det gör inget om det råkar bli fel, därför brukar vi tänka lite på hur allting gått till så att vi blir bättre till nästa gång.",
+    ],
     image: "/images/department2.png",
     text: "text-sky-500",
     border: "lg:border-sky-500",
@@ -46,8 +61,15 @@ export const DepartmentData: DepartmentItemType[] = [
     subtitle: " Äventyrare 12-14 år",
     description:
       "Vid 12 års ålder öppnas dörrarna till Äventyrarscouterna. I tre spännande år får man lära sig nyttiga saker inom sjömansskap och natur. (Tidigare hette äventyrarscout ”patrullscout”)",
-    fullDescription:
-      "Vid 12 års ålder öppnas dörrarna till Äventyrarscouterna. I tre spännande år får man lära sig nyttiga saker inom sjömansskap och natur. (Tidigare hette äventyrarscout ”patrullscout”)  Här börjar seglingen på allvar och det äbr ut i skärgården om sommaren. När vi inte seglar tränar vi navigation och gör även traditionella landscoutaktiviteter.  På vintern när det är som kallast har vi en rejäl vinterhajk då vi bor i tält. En annan höjdare är det traditionella isvaksbadet som ingen patrullscout slipper ifrån…  Konvojen har möten på torsdagar",
+    fullDescription: [
+      "Vid 12 års ålder öppnas dörrarna till Äventyrarscouterna. I tre spännande år får man lära sig nyttiga saker inom sjömansskap och natur. (Tidigare hette äventyrarscout ”patrullscout”).",
+
+      "Här börjar seglingen på allvar och det bär ut i skärgården om sommaren. När vi inte seglar tränar vi navigation och gör även traditionella landscoutaktiviteter.",
+
+      "På vintern när det är som kallast har vi en rejäl vinterhajk då vi bor i tält. En annan höjdare är det traditionella isvaksbadet som ingen patrullscout slipper ifrån…",
+
+      "Konvojen har möten på torsdagar.",
+    ],
     image: "/images/department3.png",
     text: "text-orange-600",
     border: "lg:border-orange-600",
@@ -60,8 +82,19 @@ export const DepartmentData: DepartmentItemType[] = [
     subtitle: "15+ år",
     description:
       "HSS utmanarscoutlag upphör aldrig att existera! Efter tiden som utmanarscout är man ett tätt sammansvetsat gäng. Det är inte ovanligt att man håller kontakten livet ut. (Tidigare hette denna avdelning ”seniorscout”)",
-    fullDescription:
-      "HSS utmanarscoutlag upphör aldrig att existera! Efter tiden som utmanarscout är man ett tätt sammansvetsat gäng. Det är inte ovanligt att man håller kontakten livet ut. (Tidigare hette denna avdelning ”seniorscout”)  När man kommer upp från äventyrarscouterna bildar man ett lag tillsammans med kullkamraterna. Sedan arbetar man mot att lära sig nya saker och anta svårare utmaningar för att öka sammanhållningen. Under de första åren har man ledare med sig och allt eftersom man utvecklas blir man själv ledare på de andra avdelningarna.  Utmanarscouterna seglar själva i skärgården när de uppnått tillräcklig kunskapsnivå.  Varje utmanarscoutlag hittar på sitt eget namn (av tradition slutare de på -arne och laget kallas ”fjantarne” till dess att de kommit på ett eget namn.  Mötena sker på tisdagar eller onsdagar.",
+    fullDescription: [
+      "HSS utmanarscoutlag upphör aldrig att existera! Efter tiden som utmanarscout är man ett tätt sammansvetsat gäng. Det är inte ovanligt att man håller kontakten livet ut. (Tidigare hette denna avdelning ”seniorscout”).",
+
+      "När man kommer upp från äventyrarscouterna bildar man ett lag tillsammans med kullkamraterna. Sedan arbetar man mot att lära sig nya saker och anta svårare utmaningar för att öka sammanhållningen.",
+
+      "Under de första åren har man ledare med sig och allt eftersom man utvecklas blir man själv ledare på de andra avdelningarna.",
+
+      "Utmanarscouterna seglar själva i skärgården när de uppnått tillräcklig kunskapsnivå.",
+
+      "Varje utmanarscoutlag hittar på sitt eget namn (av tradition slutar de på -arne och laget kallas ”fjantarne” till dess att de kommit på ett eget namn).",
+
+      "Mötena sker på tisdagar eller onsdagar.",
+    ],
     image: "/images/department4.png",
     text: "text-pink-700",
     border: "lg:border-pink-700",
@@ -74,8 +107,15 @@ export const DepartmentData: DepartmentItemType[] = [
     subtitle: "",
     description:
       "Genom Familjescouting får barn under åtta år tillsammans med en eller flera vuxna på ett roligt och lekfullt sätt möta scouting. Familjescouting är dessutom ett utmärkt sätt att få in fler vuxna i kåren.",
-    fullDescription:
-      "Familjescouting är till för barn under åtta år och en vuxen som barnet känner sig trygg med. Tillsammans upptäcker ni scouting genom lek, äventyr och roliga aktiviteter. Barn och vuxna är med sida vid sida och hjälps åt i aktiviteterna. Den vuxna ansvarar för barnet, men är också en viktig del av gemenskapen och deltar lika mycket som barnen. Den vuxna kan vara en förälder, mor- eller farförälder, släkting eller någon annan nära person. Fokus i Familjescouting är att barn och vuxna får uppleva saker tillsammans och stärka sin relation.",
+    fullDescription: [
+      "Familjescouting är till för barn under åtta år och en vuxen som barnet känner sig trygg med. Tillsammans upptäcker ni scouting genom lek, äventyr och roliga aktiviteter.",
+
+      "Barn och vuxna är med sida vid sida och hjälps åt i aktiviteterna. Den vuxna ansvarar för barnet, men är också en viktig del av gemenskapen och deltar lika mycket som barnen.",
+
+      "Den vuxna kan vara en förälder, mor- eller farförälder, släkting eller någon annan nära person.",
+
+      "Fokus i Familjescouting är att barn och vuxna får uppleva saker tillsammans och stärka sin relation.",
+    ],
     image: "/images/department5.png",
     text: "text-[#652482]",
     border: "lg:border-[#652482]",
