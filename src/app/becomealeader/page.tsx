71cc3c07ecbe9f210111bdef3de56cf8a2ba6188
Nathalie <nathaliebuss21@gmail.com>
2026-06-03 11:55:50 +0200

    image fix

--- src/app/becomealeader/page.tsx
+++ src/app/becomealeader/page.tsx
@@ -1,4 +1,5 @@
 import {BecomeALeaderData} from "@/data/becomeleader";
+import SectionHero from '@/components/SectionHero'
 import Button from "@/components/Button";
 import Image from "next/image";
 
@@ -7,18 +8,19 @@ const BecomeALeader = () => {
   return (
     <div className="flex flex-col bg-primary ">
       <div className="flex flex-start flex-col px-4  lg:px-36.5 lg:py-12">
-        <h2 className="text-white font-primary text-[32px] lg:text-[53px] pt-6 pb-6">{data.title}</h2>
-        <h3 className="text-secondary font-primary pb-11 text-[28px] max-w-225.25 lg:text-[32px]">
-          {data.subtitle}
-        </h3>
-        <Button content="Bli Ledare" theme="yellow" size="large" />
+        <SectionHero title="Vill du bli en av oss?" intro="Här kanske ditt livs äventyr startar. I HSS finns det spännande utmaningar för stor som liten." button={{
+          content: "BLi ledare", 
+          size:"large", redirect:"", 
+          type:"external"
+          }}/>
       </div>
       <Image
         src={data.image}
         height={1779}
         width={1152}
-        alt="4 people on a white sailboat"
-        className=" py-9 lg:w-screen lg:max-h-150 place-self-center object-cover"
+        loading="eager"
+        alt="group of scouts on a cliff infront 3 boats"
+        className=" py-9 lg:w-screen lg:max-h-120  object-cover object-[60%_75%] place-self-center rounded-2xl"
       />
       <section className=" font-primary text-left text-white text-[18px] lg:text-[24px] leading-[31.5px] lg:leading-10.5 tracking-[0.18px] lg:tracking-[0.24px] pb-10 px-4 lg:px-36.5">
         <p className="pb-6">{data.descriptionOne}</p>
--- src/data/becomeleader.ts
+++ src/data/becomeleader.ts
@@ -11,7 +11,7 @@ export const BecomeALeaderData: {
   title: "Vill du bli en av oss?",
   subtitle:
     "Här kanske ditt livs äventyr startar. I HSS finns det spännande utmaningar för stor som liten.",
-  image: "/images/022.png",
+  image: "/images/030.jpg",
   descriptionOne: `Är du sugen på scouting och segling? Tveka inte, då ska du anmäla ditt intresse till HSS. Oavsett om du är 8 eller 100 år är du välkommen. För att anmäla dig till kölistan fyller du in en intresseanmälan nedan. Vi kontaktar dig när det blir en plats ledig.`,
 
   descriptionTwo: `Som vuxen finns det utmärkta möjligheter att utvecklas som ledare hos oss. Ledarsituationen i kåren varierar, nu är vi i akut behov av ledare till Kaparna. Du behöver inte vara en erfaren seglare för att delta i vår verksamhet. Du behöver inte heller vara gammal scout för att bli ledare. Enda kravet är att du har ett genuint intresse av att jobba med ungdomar och att du är sugen på att arbeta ideellt i en välfungerande scoutkår. Kåren betalar för den utbildning du behöver för att fungera bra som scoutledare. Vi behöver alltid föräldrar som kan hjälpa till med stort som smått. Utan föräldrar ingen kår, så enkelt är det!`,
