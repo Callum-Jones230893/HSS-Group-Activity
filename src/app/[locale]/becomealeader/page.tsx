8fae9c9627baab22ddc22065d7dd2f5362330c5c
Nathalie <nathaliebuss21@gmail.com>
2026-06-03 17:46:17 +0200

    started on translation for the page

--- src/app/[locale]/becomealeader/page.tsx
+++ src/app/[locale]/becomealeader/page.tsx
@@ -1,16 +1,19 @@
 import {BecomeALeaderData} from "@/data/becomeleader";
 import SectionHero from '@/components/SectionHero'
-import Button from "@/components/Button";
+import { useTranslations } from "next-intl"
 import Image from "next/image";
 
 const BecomeALeader = () => {
+  // t("sjohumlor.description")
   const data = BecomeALeaderData;
+  const t = useTranslations('becomealeader')
   return (
     <div className="flex flex-col bg-primary ">
       <div className="flex flex-start flex-col px-4  lg:px-36.5 lg:py-12">
         <SectionHero title="Vill du bli en av oss?" intro="Här kanske ditt livs äventyr startar. I HSS finns det spännande utmaningar för stor som liten." button={{
           content: "BLi ledare", 
-          size:"large", redirect:"", 
+          size:"large", 
+          redirect:"", 
           type:"external"
           }}/>
       </div>
@@ -22,13 +25,18 @@ const BecomeALeader = () => {
         alt="group of scouts on a cliff infront 3 boats"
         className=" py-9 lg:w-screen lg:max-h-120  object-cover object-[60%_75%] place-self-center rounded-2xl"
       />
-      <section className=" font-primary text-left text-white text-[18px] lg:text-[24px] leading-[31.5px] lg:leading-10.5 tracking-[0.18px] lg:tracking-[0.24px] pb-10 px-4 lg:px-36.5">
-        <p className="pb-6">{data.descriptionOne}</p>
-        <p className="pb-6">{data.descriptionTwo}</p>
-        <p className="pb-6">{data.descriptionThree}</p>
-        <p className="pb-6">{data.descriptionFour}</p>
-        <p>{data.descriptionFive}</p>
+      <section className="font-primary text-left text-white text-[18px] lg:text-[24px] leading-[31.5px] lg:leading-10.5 tracking-[0.18px] lg:tracking-[0.24px] px-4 lg:px-36.5">
+        <p className="pb-6">{t("descriptionOne")}</p>
+        <p className="pb-6">{t("descriptionTwo")}</p>
       </section>
+
+          <div className="font-primary text-left text-white text-[18px] lg:text-[24px] lg:leading-11 pb-10 pt-4 px-4 lg:px-36.5">
+            <p>{t("descriptionThree")}</p>
+              <p className="py-2 pb-10 lg:pb-6">{t("quoteOne")}</p>
+            <p>{t("descriptionFour")}</p>
+              <p className="py-2 lg:pb-6">{t("quoteTwo")}</p>
+            <p className="pt-4">{t("descriptionFive")}</p>
+          </div>
     </div>
   );
 };
