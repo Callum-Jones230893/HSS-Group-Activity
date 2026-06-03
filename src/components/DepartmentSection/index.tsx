2a4c07103f496ce10c3cfb19bd02cb38ab5b8ed2
Oscar Öjling <oscarojling@gmail.com>
2026-06-03 11:15:56 +0200

    Changed padding and some things in hero

--- src/components/DepartmentSection/index.tsx
+++ src/components/DepartmentSection/index.tsx
@@ -18,23 +18,23 @@ const DepartmentSection = ({
   return (
     <section className="w-full font-primary">
       <div className="bg-primary/5">
-        <div className="max-w-432 mx-auto px-4 lg:px-36.75 py-10 lg:py-20">
-          <div className="bg-[#fff] rounded-2xl p-4 lg:py-8 lg:px-12 flex flex-col lg:flex-row gap-4 lg:gap-8 max-w-347.75 mx-auto">
+        <div className="max-w-432 mx-auto px-6 lg:px-30 py-6 lg:py-20">
+          <div className="bg-[#fff] rounded-2xl py-4 px-6 flex flex-col lg:flex-row gap-2 lg:gap-4 max-w-347.75 mx-auto">
             <Image
               src={image}
               alt={title}
               width={244}
               height={244}
-              className="object-contain w-29.5 h-29.5 md:h-56 md:w-56"
+              className="w-29.5 h-29.5 lg:h-56 lg:w-56"
             />
 
-            <div className="flex-1 flex flex-col">
-              <div className="flex items-center gap-1.5 mb-4">
-                <h2 className="text-primary font-extralight text-[20px] md:text-[24px]">
+            <div className="flex-1 flex flex-col lg:max-w-225">
+              <div className="flex items-center gap-2 mb-4">
+                <h2 className="text-primary font-extralight text-[20px] lg:text-[24px]">
                   {title}
                 </h2>
                 <p
-                  className={`font-extralight text-[20px] md:text-[24px] ${text}`}
+                  className={`font-extralight text-[20px] lg:text-[24px] ${text}`}
                 >
                   {subtitle}
                 </p>
@@ -42,7 +42,7 @@ const DepartmentSection = ({
               {fullDescription.map((description, index) => (
                 <p
                   key={index}
-                  className="mb-3 md:mb-4 text-[18px] font-light md:text-[24px]"
+                  className="mb-3 md:mb-4 text-[18px] font-light lg:text-[24px]"
                 >
                   {description}
                 </p>
