d9977f47f79d25b53e9261c4972c6a206cffdf54
claudiacarion <claudia.carion@gmail.com>
2026-06-04 16:07:41 +0200

    add form with desktop styling

--- src/components/ContactUsForm/index.tsx
+++ src/components/ContactUsForm/index.tsx
@@ -5,7 +5,7 @@ import Button from "../Button";
 const ContactUsForm = () => {
   const t = useTranslations("contactus");
   return (
-    <div className="bg-primary relative h-95 font-primary">
+    <div className="bg-primary relative font-primary border py-8">
       <Image
         src="/images/background_shell1.png"
         height={300}
@@ -15,23 +15,27 @@ const ContactUsForm = () => {
       />
       <div className="mx-auto w-357">
         <h2 className="text-secondary text-[53px]">{t("title")}</h2>
-        <h3 className="text-white text-[42px]">{t("subtitle")}</h3>
-        <div className='w-1/2'>
-          <form action="mailto:" method="POST" encType="text/plain" className='text-white'>
+        <h3 className="text-white text-[36px]">{t("subtitle")}</h3>
+        <div className="w-1/2 py-10 mx-auto">
+          <form action="mailto:" method="POST" encType="text/plain" className="text-white">
             <fieldset>
-              <div>
-                <label htmlFor="name">{t("fieldOne")}</label>
-                <input type="text" name="name" id="name" required className='border-b' />
+              <div className="flex flex-col mb-8">
+                <label htmlFor="name">{t("fieldOne")}*</label>
+                <input type="text" name="name" id="name" required className="border-b my-1 p-1" />
               </div>
-              <div>
-                <label htmlFor="email">{t("fieldTwo")}</label>
-                <input type="email" name="email" id="email" required border-b/>
+              <div className="flex flex-col mb-8">
+                <label htmlFor="email">{t("fieldTwo")}*</label>
+                <input type="email" name="email" id="email" required className="border-b my-1 p-1" />
               </div>
-              <div>
-                <label htmlFor="message">{t("fieldThree")}</label>
-                <textarea name="message" id="message" className="message-area" required border-b></textarea>
+              <div className="flex flex-col mb-8 my-1">
+                <label htmlFor="message">{t("fieldThree")}*</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  required
+                  className="border-b h-25 overflow-y-auto resize-none p-1"></textarea>
               </div>
-              <Button content={t("button")} size="large" type="mail" redirect='' />
+              <Button content={t("button")} size="large" type="mail" redirect="" />
             </fieldset>
           </form>
         </div>
