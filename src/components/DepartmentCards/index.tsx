cfb778f41eba951b6819e076f7e63b5cdfaaa777
Callum Jones <call_jones2@hotmail.com>
2026-06-04 16:18:35 +0200

    updated

--- src/components/DepartmentCards/index.tsx
+++ src/components/DepartmentCards/index.tsx
@@ -21,18 +21,12 @@ const DepartmentCards = ({item}: DepartmentItemProps) => {
           <Image src={item.image} width="400" height="400" alt={t(`${item.id}.title`)} loading="eager" className="size-28.5 xl:size-60 rounded-2xl object-contain"/>
         </div>
         <div className="flex flex-col my-3 xl:mx-11.5 justify-evenly">
-          <div className="flex items-center gap-1.5">
-            <h3 className="font-extralight text-2xl xl:text-3xl text-sky-900">{t(`${item.id}.title`)}</h3>
-            <p className={`font-bold text-1xl ${item.text}`}>{t(`${item.id}.subtitle`)}</p>
-          </div>
-          <div className="flex flex-col my-3 xl:mx-11.5 justify-evenly">
-            <div className="flex items-center gap-1.5">
-              <h3 className="font-extralight text-2xl xl:text-3xl text-sky-900">{t(`${item.id}.title`)}</h3>
-              <p className={`font-bold text-1xl ${item.text}`}>{t(`${item.id}.subtitle`)}</p>
-            </div>
-            <div className="flex justify-start pt-4.5 xl:pr-20 font-light text-slate-900 self-stretch leading-8 xl:leading-10 tracking-tight">
-              <p>{t(`${item.id}.description`)}</p>
-            </div>
+        <div className="flex items-center gap-1.5">
+          <h3 className="font-extralight text-2xl xl:text-3xl text-sky-900">{t(`${item.id}.title`)}</h3>
+          <p className={`font-bold text-1xl ${item.text}`}>{t(`${item.id}.subtitle`)}</p>
+        </div>
+          <div className="flex justify-start pt-4.5 xl:pr-20 font-light text-slate-900 self-stretch leading-8 xl:leading-10 tracking-tight">
+            <p>{t(`${item.id}.description`)}</p>
           </div>
         </div>
       </div>
