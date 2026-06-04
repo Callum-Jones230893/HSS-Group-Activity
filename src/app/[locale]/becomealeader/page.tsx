c0c07be003c402f211e88a967a0cd1c68e7d1e92
Nathalie <nathaliebuss21@gmail.com>
2026-06-04 10:20:11 +0200

    finished translations

--- src/app/[locale]/becomealeader/page.tsx
+++ src/app/[locale]/becomealeader/page.tsx
@@ -4,7 +4,6 @@ import { useTranslations } from "next-intl"
 import Image from "next/image";
 
 const BecomeALeader = () => {
-  // t("sjohumlor.description")
   const data = BecomeALeaderData;
   const t = useTranslations('becomealeader')
   return (
