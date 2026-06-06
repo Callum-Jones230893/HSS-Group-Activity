d628be2eb8307cf7e0319386d8d1033f03636247
claudiacarion <claudia.carion@gmail.com>
2026-06-06 19:22:40 +0200

    add link to dept page on about us page

--- src/app/[locale]/aboutus/page.tsx
+++ src/app/[locale]/aboutus/page.tsx
@@ -1,5 +1,18 @@
+import { useTranslations } from "next-intl";
 import AboutSection from "@/components/AboutSection";
+import Button from "@/components/Button";
+
 const About = () => {
-  return <AboutSection />;
+  const t = useTranslations("aboutus");
+
+  return (
+    <>
+      <AboutSection />
+      <section className="w-full max-w-432.5 mx-auto bg-white pb-10 px-4 lg:px-20 xl:px-37.75 font-primary overflow-none">
+        <h3 className="text-3xl text-primary font-primary pb-4">{t("linkTitle")}</h3>
+        <Button content={t("button")} size="large" type="redirect" url="" redirect="/becomeascout" />
+      </section>
+    </>
+  );
 };
 export default About;
