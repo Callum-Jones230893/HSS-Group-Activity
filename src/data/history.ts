14d7703f07740acccfeefdd81f1e8b68c7d384d3
Callum Jones <call_jones2@hotmail.com>
2026-06-07 16:37:02 +0200

    first commit, added translations from figma and initial folders

---
+++ src/data/history.ts
@@ -0,0 +1,27 @@
+export type HistoryType = {
+  id: string
+  image: string
+}
+
+export const HistoryData: HistoryType[] = [
+  {
+    id: "intro",
+    image: ""
+  },
+  {
+    id: "boatHistory",
+    image: ""
+  },
+  {
+    id: "buildingMyset",
+    image: ""
+  },
+  {
+    id: "expanding",
+    image: ""
+  },
+  {
+    id: "today",
+    image: ""
+  }
+]
