71ecb6c5f33b31650a590e0a9781b0f92a5b0ccf
Callum Jones <call_jones2@hotmail.com>
2026-06-10 21:08:10 +0200

    adjusted image scaling

--- src/components/HistoryCards/index.tsx
+++ src/components/HistoryCards/index.tsx
@@ -23,36 +23,40 @@ const HistoryCards = ({item, first, expanded, updateExpanded, updateImage, close
   }
 
   return (
-    <div className="flex flex-col lg:flex-row p-8 2xl:w-8/10">
-      <div className="flex flex-col md:mx-8 lg:pr-12 xl:w-8/10 text-[18px] md:text-[24px] text-primary tracking-[1%] gap-4.5 leading-[175%] mb-5">
+    <section className="flex flex-col lg:flex-row p-8 2xl:w-8/10">
+      <article className="flex flex-col md:mx-8 lg:pr-12 xl:w-8/10 text-[18px] md:text-[24px] text-primary tracking-[1%] gap-4.5 leading-[175%] mb-5">
         <h2 className="text-[20px] md:text-[36px] text-black">{t(`${item.id}.date`)}</h2>
         <h1 className="text-[24px] md:text-[44px] font-light">{t(`${item.id}.title`)}</h1>
         <p>{t(`${item.id}.description`)}</p>
         <p>{t(`${item.id}.descriptionTwo`)}</p>
-      </div>
-      <div className={`${first ? "justify-center" : "flex-col" } flex items-start mx-auto w-full lg:min-w-100 max-w-150`}>
-        <Image src={item.image} width="400" height="400" alt={t(`${item.id}.title`)} loading="eager" 
-          className={`${first ? "w-50 md:w-75 aspect-square object-contain" : "w-150 aspect-3/2"}`}
-        />
-        {!first &&
-          <div className="mx-auto cursor-pointer fill-primary hover:scale-112 hover:fill-secondary transition duration-300"
-            ref={closeOverlay as React.RefObject<HTMLDivElement>}
-            onClick={!first ? imagehelper : undefined}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" className="w-7.5 aspect-square mx-auto mt-5">
-              <path d="
-                M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 
-                554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9
-                480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64
-                480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 
-                128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z"
-              />
-            </svg>
-          </div>
-        }
-      </div>
-    </div>
+      </article>
+      <div>
+        <div className={`${first ? "justify-center" : "" } flex items-center mx-auto w-full sm:h-100 lg:w-200 lg:h-100 object-contain overflow-hidden max-w-150`}>
+          <Image src={item.image} width="400" height="400" alt={t(`${item.id}.title`)} loading="eager"
+            className={`${first ? "w-50 md:w-75 aspect-square object-contain" : "w-150"}`}
+          />
+        </div>
+          {!first &&
+            <div className="mx-auto cursor-pointer fill-primary hover:scale-112 hover:fill-secondary transition duration-300"
+              ref={closeOverlay as React.RefObject<HTMLDivElement>}
+              onClick={!first ? imagehelper : undefined}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" className="w-7.5 aspect-square mx-auto mt-5">
+                <path d="
+                  M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 
+                  554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9
+                  480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64
+                  480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 
+                  128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z"
+                />
+              </svg>
+            </div>
+          }
+        </div>
+    </section>
   )
 }
 
 export default HistoryCards
+
+ 
