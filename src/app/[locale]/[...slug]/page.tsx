7e5eecfb6d40c5afff6a4defe20c9546f8f18ceb
claudiacarion <claudia.carion@gmail.com>
2026-06-10 10:25:47 +0200

    remove commented code in slug

--- src/app/[locale]/[...slug]/page.tsx
+++ src/app/[locale]/[...slug]/page.tsx
@@ -1,10 +1,5 @@
-// import { redirect } from "next/navigation";
-// import { getLocale } from 'next-intl/server';
 import { notFound } from 'next/navigation'
 
 export default async function Page() {
-  // const locale = await getLocale()
-
-  // redirect(`/${locale}/404`);
   notFound()
 }
