460df7c97f92134e554b2e871a5f7672eb4e7c1e
Oscar Öjling <oscarojling@gmail.com>
2026-06-11 17:41:15 +0200

    Making progress

--- src/components/DepartmentSection/index.tsx
+++ src/components/DepartmentSection/index.tsx
@@ -9,7 +9,8 @@ type DepartmentItemProps = {
 
 const DepartmentSection = ({ item }: DepartmentItemProps) => {
   const t = useTranslations("departments");
-  const tBack = useTranslations()
+  const tBack = useTranslations();
+  const tFacts = useTranslations("departmentFacts");
 
   return (
     <section className="w-full font-primary">
@@ -38,6 +39,24 @@ const DepartmentSection = ({ item }: DepartmentItemProps) => {
               <p className="mb-4 whitespace-pre-line text-[18px] leading-relaxed font-light lg:text-[24px]">
                 {t(`${item.id}.fullDescription`)}
               </p>
+              <div className="bg-primary/5 text-primary rounded-2xl p-6 mb-6 grid grid-cols-2 lg:grid-cols-4 gap-4">
+                <div>
+                  <p className="text-xs mb-1">{tFacts("age")}</p>
+                  <p className="text-sm font-medium">{t(`${item.id}.age`)}</p>
+                </div>
+                 <div>
+                  <p className="text-xs mb-1">{tFacts("meetings")}</p>
+                  <p className="text-sm font-medium">{t(`${item.id}.meetings`)}</p>
+                </div>
+                 <div>
+                  <p className="text-xs mb-1">{tFacts("place")}</p>
+                  <p className="text-sm font-medium">{t(`${item.id}.location`)}</p>
+                </div>
+                 <div>
+                  <p className="text-xs mb-1">{tFacts("learn")}</p>
+                  <p className="text-sm font-medium">{t(`${item.id}.learn`)}</p>
+                </div>
+              </div>
               <div className="mb-4">
                 <Link
                   href="/becomeascout"
