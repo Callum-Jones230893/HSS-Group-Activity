1c5835a0d9dfa248306eed1769b997d8c1e36ae2
Callum Jones <call_jones2@hotmail.com>
2026-06-15 10:50:27 +0200

    added more consistency

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,17 +1,17 @@
-import {getRequestConfig} from 'next-intl/server';
-import {hasLocale} from 'next-intl';
-import {routing} from './routing';
+import { getRequestConfig } from "next-intl/server";
+import { hasLocale } from "next-intl";
+import { routing } from "./routing";
 
-export default getRequestConfig(async ({requestLocale}) => {
+export default getRequestConfig(async ({ requestLocale }) => {
   const locale = await requestLocale;
 
   const currentLocale =
     locale && hasLocale(routing.locales, locale)
       ? locale
-      : routing.defaultLocale
+      : routing.defaultLocale;
 
   return {
     locale: currentLocale,
-    messages: (await import(`../translations/${currentLocale}.json`)).default
-  }
-})
+    messages: (await import(`../translations/${currentLocale}.json`)).default,
+  };
+});
